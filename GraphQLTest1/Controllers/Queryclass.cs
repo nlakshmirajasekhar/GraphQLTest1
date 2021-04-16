b5a8937c40d93906c19e7ee1338eb10dffbbb4f9
nlakshmirajasekhar <nlakshmirajasekhar@gmail.com>
2021-04-16 12:35:41 +0530

    atlast changes

--- GraphQLTest1/Controllers/Queryclass.cs
+++ GraphQLTest1/Controllers/Queryclass.cs
@@ -44,18 +44,38 @@ namespace GraphQLTest1.Controllers
      
 
         // returning the purchase header and purchase lines based on Mir
-        public purchasewrapper GetPurchaselines([Service] ShopInventory1Context db,int MIR,purchasewrapper pwr)
+        public purchasewrapper GetPurchaselinesbyID([Service] ShopInventory1Context db,int MIR,purchasewrapper pwr)
         {
             pwr.purchasesheader = db.Purchasesheader.Where(a => a.Mir == MIR).FirstOrDefault();
             pwr.purchaseslines = db.Purchaseslines.Where(a => a.Mir == MIR).ToList();
             return pwr;
         }
+        public purchasewrapper GetPurchaselines([Service] ShopInventory1Context db, purchasewrapper pwr)
+        {
+            var size = db.Purchasesheader.Count();
+            for(int j=0;j<=size;j++)
+            {
+                pwr.purchasesheader = db.Purchasesheader.Where(a => a.Mir == j).FirstOrDefault();
+                pwr.purchaseslines = db.Purchaseslines.Where(a => a.Mir == j).ToList();
+            }
+            return pwr;
+        }
 
-        //returning the sales header and sales lines based on Billno
-        public salesWrapper GetSaleslines([Service] ShopInventory1Context db, int Bill, salesWrapper swr)
+            //returning the sales header and sales lines based on Billno
+            public salesWrapper GetSaleslineByid([Service] ShopInventory1Context db, int Bill, salesWrapper swr)
         {
             swr.salesheader = db.Salesheader.Where(a => a.Billno == Bill).FirstOrDefault();
             swr.salesline = db.Saleslines.Where(a => a.Billno == Bill).ToList();
+            return swr;
+        }
+        public SalesWrapper GetSaleslines([Service] ShopInventory1Context db, SalesWrapper swr)
+        {
+            var size = db.Salesheader.Count();
+             for(int i=1; i<=size;i++)
+            {
+                swr.salesheader = db.Salesheader.Where(a => a.Billno == i).FirstOrDefault();
+                swr.saleslines = db.Saleslines.Where(a => a.Billno == i).ToList();
+            }
             return swr;
         }
 
